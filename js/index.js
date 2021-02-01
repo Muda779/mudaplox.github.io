273c993d185bdfa7617061f5fedff69917785b1a
Muda <56939662+Muda779@users.noreply.github.com>
2021-02-01 16:06:05 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,40 @@
+let darkMode = localStorage.getItem("darkMode");
+let icon = document.createElement("i")
+
+function enableDarkMode() {
+    let link=document.createElement("link");
+  link.setAttribute("rel", "stylesheet");
+  link.setAttribute("type", "text/css");
+  link.setAttribute("href", "css/light-mode.css");
+  document.getElementsByTagName("head")[0].appendChild(link);
+  icon.setAttribute("class", "fas fa-sun")
+  icon.setAttribute("id", "dark-mode-toggle")
+  document.getElementById("dark-mode-toggle").appendChild(icon)
+  document.body.classList.add("darkmode");
+  localStorage.setItem("darkMode", "enabled");
+}
+
+function disableDarkMode() {
+  let link=document.createElement("link");
+  link.setAttribute("rel", "stylesheet");
+  link.setAttribute("type", "text/css");
+  link.setAttribute("href", "css/main.css");
+  document.getElementsByTagName("head")[0].appendChild(link);
+  icon.setAttribute("class", "fas fa-moon")
+  icon.setAttribute("id", "dark-mode-toggle")
+  document.getElementById("dark-mode-toggle").appendChild(icon)
+  document.body.classList.remove("darkmode");
+  localStorage.setItem("darkMode", null);
+}
+
+if (darkMode === "enabled") {
+  enableDarkMode();
+} else {
+    disableDarkMode()
+}
+
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
+darkModeToggle.addEventListener("click", () => {
+  darkMode = localStorage.getItem("darkMode");
+  darkMode !== "enabled" ? enableDarkMode() : disableDarkMode();
+});
---
+++ js/tippy.js
@@ -0,0 +1,20 @@
+tippy('#python', {
+  animateFill: true,
+  allowHTML: true,
+  content: '30%',
+});
+tippy('#js', {
+  animateFill: true,
+  allowHTML: true,
+  content: '60%',
+});
+tippy('#html', {
+  animateFill: true,
+  allowHTML: true,
+  content: '30%',
+});
+tippy('#css', {
+  animateFill: true,
+  allowHTML: true,
+  content: '30%',
+});
